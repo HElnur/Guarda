f59a6b250f0486eb97850bb54b80606448d559fc
HElnur <huseynove174@gmail.com>
2022-06-16 02:02:14 +0400

    Add comment configuration and validation

---
+++ Back-end/Guarda/Guarda.Business/DTOs/Comment/CommentPostDto.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guarda.Business.DTOs.Comment
+{
+    public class CommentPostDto
+    {
+        public string Text { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public bool Status { get; set; }
+        public int Rate { get; set; }
+    }
+
+    public class CommentPostDtoValidator : AbstractValidator<CommentPostDto>
+    {
+        public CommentPostDtoValidator()
+        {
+            RuleFor(x => x.Text)
+                .NotEmpty();
+
+            RuleFor(x => x.Rate)
+                .GreaterThanOrEqualTo(1)
+                .LessThanOrEqualTo(5);
+        }
+    }
+}
