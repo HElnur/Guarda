0f983403bc4175e6376ab131d4a1b9b1d85395c0
HElnur <huseynove174@gmail.com>
2022-06-19 21:21:47 +0400

    Add services entity and configuration

---
+++ Back-end/Guarda/Guarda.Data/Configurations/ServiceConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Guarda.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guarda.Data.Configurations
+{
+    public class ServiceConfiguration : IEntityTypeConfiguration<Services>
+    {
+        public void Configure(EntityTypeBuilder<Services> builder)
+        {
+            builder.Property(s => s.Name).HasMaxLength(50).IsRequired();
+
+            builder.Property(s => s.Description).HasMaxLength(50).IsRequired();
+
+            builder.Property(s => s.Image).HasMaxLength(100);
+
+            builder.Ignore(s => s.ImageFile);
+        }
+    }
+}
